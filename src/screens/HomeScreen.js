5002c5a9697f5d6ddd40ff056d0d1061caa18c0c
Kuan-Wei Huang <huang.kua@husky.neu.edu>
2020-03-01 21:38:16 -0800

    change to expo and add search bar

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+const HomeScreen = () => {
+    const [term, setTerm] = useState('');
+
+    return (
+        <View>
+            <SearchBar 
+                term={term} 
+                onTermChange={(newTerm) => setTerm(newTerm)}
+                onTermSubmit={() => console.log('Term was submitted')}
+            />
+            <Text>Home Screen</Text>
+            <Text>{term}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
