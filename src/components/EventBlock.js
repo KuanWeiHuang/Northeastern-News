baf4bf35dc2ce1a89aeb1ada4be15042a5a486c9
001029471_Min <48940875+Evelyn0116@users.noreply.github.com>
2020-03-15 00:51:42 -0700

    mar 15

--- src/components/EventBlock.js
+++ src/components/EventBlock.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons } from "@expo/vector-icons";
+import { withNavigation } from "react-navigation";
+import { Feather, Entypo, MaterialIcons } from "@expo/vector-icons";
+const reducer = (state, action) => {
+  //action={type:'like' || 'mark'}
+  switch (action.type) {
+    case "like":
+      if (state.like) {
+        return { ...state, like: (state.like = false) };
+      } else {
+        return { ...state, like: (state.like = true) };
+      }
 
-const EventBlock = props => {
-  console.log(props);
+    case "mark":
+      if (state.mark) {
+        return { ...state, mark: (state.mark = false) };
+      } else {
+        return { ...state, mark: (state.mark = true) };
+      }
+    default:
+      return state;
+  }
+};
+
+const EventBlock = ({
+  day,
+  year,
+  title,
+  time,
+  describe,
+  linkScreen,
+  navigation
+}) => {
+  const [state, dispatch] = useReducer(reducer, {
+    like: false,
+    mark: false
+  });
+
   return (
     <SafeAreaView style={{ height: 250 }}>
       <View style={styles.container}>
         <View>
-          <Text style={styles.day}>{props.day}</Text>
-          <Text style={styles.day}>{props.year}</Text>
+          <Text style={styles.day}>{day}</Text>
+          <Text style={styles.day}>{year}</Text>
         </View>
 
         <View style={{ alignItems: "center" }}>
@@ -20,24 +53,53 @@ const EventBlock = props => {
 
         <View style={styles.block}>
           {/* text and image block */}
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate(`${linkScreen}`)}
+          >
             <View style={styles.detailText}>
-              <Text style={styles.detailTitle}>{props.title}</Text>
-              <Text style={styles.detailTime}>{props.time}</Text>
-              <Text>{props.describe}</Text>
+              <Text style={styles.detailTitle}>{title}</Text>
+              <Text style={styles.detailTime}>{time}</Text>
+              <Text>{describe}</Text>
             </View>
           </TouchableOpacity>
 
           {/* icons */}
           <View style={styles.iconBlock}>
             <View style={styles.tag}>
-              <Ionicons name="ios-checkmark" size={30} />
+              <Feather name="tag" size={25} style={styles.icon} />
               <Text> Events</Text>
             </View>
-            <View style={styles.likes}>
-              <Ionicons name="ios-checkmark" size={30} />
-              <Ionicons name="ios-checkmark" size={30} />
-              <Ionicons name="ios-checkmark" size={30} />
+            {/* right three icon  */}
+            <View style={styles.threeIcons}>
+              <TouchableOpacity onPress={() => dispatch({ type: "like" })}>
+                <View style={{ width: 25, height: 25 }}>
+                  {state.like ? (
+                    <Entypo name="heart" size={25} color="#E22C2C" />
+                  ) : (
+                    <Entypo
+                      name="heart-outlined"
+                      size={25}
+                      style={styles.icon}
+                    />
+                  )}
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => dispatch({ type: "mark" })}>
+                <View style={{ width: 25, height: 25 }}>
+                  {state.mark ? (
+                    <MaterialIcons name="bookmark" size={25} color="#FCA42D" />
+                  ) : (
+                    <MaterialIcons
+                      name="bookmark-border"
+                      size={25}
+                      style={styles.icon}
+                    />
+                  )}
+                </View>
+              </TouchableOpacity>
+
+              <Feather name="share" size={22} style={styles.icon} />
             </View>
           </View>
         </View>
@@ -111,20 +173,18 @@ const styles = StyleSheet.create({
   iconBlock: {
     flexDirection: "row",
     width: 280
-    // justifyContent: "space-around",
     // borderColor: "blue",
     // borderWidth: 1
   },
   tag: {
     flex: 1,
     flexDirection: "row",
-    // marginHorizontal: 5,
     justifyContent: "center",
     alignItems: "center"
     // borderColor: "red",
     // borderWidth: 1
   },
-  likes: {
+  threeIcons: {
     flex: 1,
     left: 10,
     flexDirection: "row",
@@ -132,7 +192,10 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly"
     // borderColor: "green",
     // borderWidth: 1
+  },
+  icon: {
+    opacity: 0.8
   }
 });
 
-export default EventBlock;
+export default withNavigation(EventBlock);
