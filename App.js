ee4ba16d647a72ba473c3f2c9b496e194ba27d0c
XinyiDai1017 <daixinyi96@gmail.com>
2020-03-20 00:07:22 -0700

    add the bottom nav bar with icon

--- App.js
+++ App.js
@@ -1,29 +1,49 @@
 import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import SigninScreen from './src/screens/SigninScreen';
 import HomeScreen from './src/screens/HomeScreen';
-import NewsScreen from './src/screens/NewsScreen';
-import FavoritesScreen from './src/screens/FavoritesScreen'
-import { Provider } from './src/context/Favorites'
+import FavoritesScreen from './src/screens/FavoritesScreen';
+import { Ionicons } from '@expo/vector-icons';
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    News: NewsScreen,
-    Favorites: FavoritesScreen
-  },
-  {
-    initialRouteName: 'Home',
-    defaultNavigationOptions: {
-      title: 'NEU Bay Area'
+const switchNavigator = createSwitchNavigator({
+  mainFlow: createBottomTabNavigator({
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name="ios-home" size={25} color={tabInfo.tintColor}/>;
+        }
+      }
+    },
+
+    Favorite: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarLabel: "Favorites",
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name="ios-bookmark" size={25} color={tabInfo.tintColor} />;
+        }
+      }
+    },
+
+    Signin: {
+      screen: SigninScreen,
+      navigationOptions: {
+        tabBarLabel: "Profile",
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name="ios-settings" size={25} color={tabInfo.tintColor}/>;
+        }
+      }
     }
-  }
-);
-
-const App = createAppContainer(navigator);
-
-export default () => {
-    return <Provider>
-        <App />
-    </Provider>
-};
+    },
+    {
+      tabBarOptions:{
+        activeTintColor: 'darkred'
+      }
+  })
+});
+  
+export default createAppContainer(switchNavigator);
