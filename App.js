3009878100dac5a3e91b489816d146223e393cdf
XinyiDai1017 <daixinyi96@gmail.com>
2020-03-22 17:34:52 -0700

    solve header and bottomTab bug

--- App.js
+++ App.js
@@ -1,53 +1,65 @@
 import React from 'react';
 import firebase from 'firebase';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer , createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SigninScreen from './src/screens/SigninScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import NewsScreen from './src/screens/NewsScreen';
 import FavoritesScreen from './src/screens/FavoritesScreen'
 import { Provider } from './src/context/Favorites';
-import FavoritesScreen from './src/screens/FavoritesScreen';
 import { Ionicons } from '@expo/vector-icons';
 
-const switchNavigator = createSwitchNavigator({
-  mainFlow: createBottomTabNavigator({
-    Home: {
-      screen: HomeScreen,
-      navigationOptions: {
-        tabBarLabel: "Home",
-        tabBarIcon: (tabInfo) => {
-          return <Ionicons name="ios-home" size={25} color={tabInfo.tintColor}/>;
-        }
-      }
-    },
 
-    Favorite: {
-      screen: FavoritesScreen,
-      navigationOptions: {
-        tabBarLabel: "Favorites",
-        tabBarIcon: (tabInfo) => {
-          return <Ionicons name="ios-bookmark" size={25} color={tabInfo.tintColor} />;
-        }
-      }
-    },
+const App = createStackNavigator(
+  {
 
-    Signin: {
-      screen: SigninScreen,
-      navigationOptions: {
-        tabBarLabel: "Profile",
-        tabBarIcon: (tabInfo) => {
-          return <Ionicons name="ios-settings" size={25} color={tabInfo.tintColor}/>;
-        }
+    Home: HomeScreen,
+    News: NewsScreen,
+    Favorites: FavoritesScreen
+  },
+  {
+//initial rountine deleted
+//provider deleted
+    defaultNavigationOptions:{
+      title: 'NEU Bay Area'
+    }
+  }
+);
+
+const BottomNavigation = createBottomTabNavigator({
+  Home: {
+    screen: App,
+    navigationOptions: {
+      tabBarLabel: "Home",
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name="ios-home" size={25} color={tabInfo.tintColor}/>;
       }
     }
-    },
-    {
-      tabBarOptions:{
-        activeTintColor: 'darkred'
+  },
+
+  Favorite: {
+    screen: FavoritesScreen,
+    navigationOptions: {
+      tabBarLabel: "Favorites",
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name="ios-bookmark" size={25} color={tabInfo.tintColor} />;
       }
-  })
+    }
+  },
+
+  Signin: {
+    screen: SigninScreen,
+    navigationOptions: {
+      tabBarLabel: "Profile",
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name="ios-settings" size={25} color={tabInfo.tintColor}/>;
+      }
+  }}
+},{
+    tabBarOptions:{
+      activeTintColor: 'darkred'
+  }
 });
-  
-export default createAppContainer(switchNavigator);
+
+export default createAppContainer(BottomNavigation);
