fc650927995bf29eb65975d7814d7d2140dc3a22
001029471_Min <48940875+Evelyn0116@users.noreply.github.com>
2020-03-24 14:16:33 -0700

    change event order

--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -10,6 +10,24 @@ const EventScreen = () => {
         <View style={styles.line}></View>
         <View>
           <EventBlock
+            day="Mar 20"
+            year="2020"
+            title="Free Live Leetcode Practice"
+            time="5:30-6:30 pm"
+            describe="For those of you seeking to improve your technical interview skills for Co-op or full-time roles, this series of workshops will definitely be helpful."
+            linkScreen="Event3"
+            imagePath=""
+          />
+          <EventBlock
+            day="Mar 18"
+            year="2020"
+            title="Amazon Interview 101 Workshop(Webinar)"
+            time="4:30-5:00 pm"
+            describe="This webinar will focus on detailing how Amazon's onsite interview loop works and how to “ace it”. Insight will be shared into the behavioral portions of the interview, which often “make or break” candidates."
+            linkScreen="Event2"
+            imagePath=""
+          />
+          <EventBlock
             day="Mar 12"
             year="2020"
             title="Technical Interview Workshp with Michelle Lu"
@@ -17,24 +35,6 @@ const EventScreen = () => {
             describe="This workshop will teach students will how to examine and answer a data analyst interview question."
             linkScreen="Event1"
             imagePath="ImagesLib.event1"
-          />
-          <EventBlock
-            day="Mar 18"
-            year="2020"
-            title="Amazon Interview 101 Workshop(Webinar)"
-            time="4:30-5:00 pm"
-            describe="This webinar will focus on detailing how Amazon's onsite interview loop works and how to “ace it”"
-            linkScreen="Event2"
-            imagePath=""
-          />
-          <EventBlock
-            day="Mar 20"
-            year="2020"
-            title="Free Live Leetcode Practice"
-            time="4:30-5:00 pm"
-            describe="For those of you seeking to improve your technical interview skills for Co-op or full-time roles, this series of workshops will definitely be helpful."
-            linkScreen="Event3"
-            imagePath=""
           />
         </View>
       </ScrollView>
@@ -50,7 +50,7 @@ const styles = StyleSheet.create({
   line: {
     width: 1,
     backgroundColor: "#9b9b9b",
-    marginLeft: 95,
+    marginLeft: 96,
     ...StyleSheet.absoluteFillObject
   }
 });
